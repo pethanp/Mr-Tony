76c401e3e90979534c2bd1b3e2ace214b737da1f
Jared Grambihler <jaredgrambihler@gmail.com>
2020-12-03 19:04:57 -0800

    add strip, exception for no file, print key

--- bot.py
+++ bot.py
@@ -19,17 +19,18 @@ class MyClient(discord.Client):
             await message.channel.send(f'MESSAGE: {message.content}')
 
 def get_key():
-    
+
     try:
         fin = open("KEYS.txt", 'r')
-        ftext = fin.read()
+        ftext = fin.read().strip()
         fin.close()
         return ftext
         
     except:
-        print("!Key file not found!.\n")
-        return ''
+        raise FileNotFoundError("!Key file not found!.\n")
 
 client = MyClient()
 
-client.run(get_key)
+key = get_key()
+print(f"Key is: {key}")
+client.run(key)
