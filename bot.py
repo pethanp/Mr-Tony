3da4fd1236b5deac6569bdd019965db6abdb710b
Jared Grambihler <jaredgrambihler@gmail.com>
2020-12-04 19:25:01 -0800

    Add methods to log user leave / join

--- bot.py
+++ bot.py
@@ -1,4 +1,6 @@
-
+import os
+import datetime
+import pandas as pd
 
 import discord
 from discord.ext import commands
@@ -28,11 +30,47 @@ async def on_member_update(before, after):
     # check name changes here
     pass
 
+
+def log_voice_state_update(member, before, after):
+    logFile = "testLogs/voiceUpdates.csv"
+    if not os.path.isfile(logFile):
+        df = pd.DataFrame(columns = ["time", "user_id", "user_name", "join", "leave", "channel_id", "channel_name"])
+    else:
+        df = pd.read_csv(logFile)
+    userId = member.id
+    userName = member.name
+    if before.channel != after.channel:
+        join = True
+    else:
+        join = False
+    if before.channel is not None and after.channel is None:
+        leave = True
+    else:
+        leave = False
+    if after.channel:
+        # channel_id = after.channel.channel_id
+        channel_id = None
+        channel_name = after.channel.name
+    else:
+        channel_id = None
+        channel_name = None
+    d = {"time": datetime.datetime.now(),
+         "user_id":userId,
+         "user_name": userName,
+         "join": join,
+         "leave" : leave,
+         "channel_id": channel_id,
+         "channel_name": channel_name
+        }
+    print(f"Adding {d} to voice logs")
+    df = df.append(d, ignore_index = True)
+    df.to_csv(logFile, index=False)
+    
+
 @client.event
 async def on_voice_state_update(member, before, after):
-    print("Member: {}".format(member))
-    print(f"Before {before}")
-    print(f"After {after}")
+    log_voice_state_update(member, before, after)
+
 
 @client.command()
 async def userTime(ctx, name):
